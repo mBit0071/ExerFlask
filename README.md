a546f9dcfd3b47a4063fb9d05c8aad3b323c9b1c
mBit0071 <130bit071@gmail.com>
2020-04-26 14:07:33 -0700

    testPage to compare GET vs POST

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ExerFlask
 Visualize exercise progress using Flask and ChartJS
+
+## Feed in 
+Data from OneNote pages
+
+## ToDo
+- [ ] Get method vs POST method
+- [ ] Do we need AJAX
---
+++ Sol/websw.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, Markup, request, flash
+from flask_inputs import Inputs
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms.validators import DataRequired
+
+import pandas as pd
+import csv
+import urllib2
+
+app = Flask(__name__)
+app.secret_key = 'EPPbPMgMm5uZ' #haha not so secret!
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+    
+@app.route('/test1', methods=['POST'])
+def testText():
+	txt = request.form['lb2']
+	txt=[txt]
+	return render_template('test1.html', title='ExerTest', text=txt)
